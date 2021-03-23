4cb8a30c47fdf62504bd511e090311293325640d
JowenPang <jowenpg@gmail.com>
2021-03-23 23:52:29 +0800

    commit from jowen

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
     var questions=[
-      "You are here to ?? :V",
+      "You are here to ?? :V   don't play food",
       "Having excess food to donate?"];
 
     return MaterialApp(
